6acbc74d6644e5b26d5cd6bf8f27ea557fe98225
andrej-koman <andrej.koman123@gmail.com>
2023-05-06 15:09:28 +0200

    feat: Basic treeview contextmenu implementation

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import TreeView from "./components/ui/TreeView.svelte";
   import TreeAdd from "./components/ui/TreeAdd.svelte";
   import type { Tree } from "./types";
-  import { onMount } from "svelte";
   let tree: Tree = {
 	label: "root"
   };
@@ -11,7 +10,6 @@
     console.log("Im here");
   }
 
-  document.addEventListener('contextmenu', event => event.preventDefault());
   
 </script>
 
@@ -22,15 +20,14 @@
 </main>
 
 <style>
-
   .treeview-container {
     background-color: var(--primary-color-light);
     width: 100%;
     border-radius: 10px;
     padding: 1rem;
-	/* Add Box Shadow*/
-	box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-	padding-left: 10px;
-	padding-right: 0px;
+	  /* Add Box Shadow*/
+	  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+	  padding-left: 10px;
+	  padding-right: 0px;
   }
 </style>
--- src/components/ui/TreeView.svelte
+++ src/components/ui/TreeView.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import type { Tree } from "../../types";
+  import { createEventDispatcher } from "svelte";
   export let tree: Tree;
   export let depth = 0;
   export let onClick;
+
+  const dispatch = createEventDispatcher();
 
   const { label, children } = tree;
   let { expanded = true } = tree;
@@ -13,9 +16,44 @@
       onClick(tree, depth, expanded);
     }
   }
+
+  const handleContextMenu = (e: MouseEvent) => {
+    e.preventDefault();
+
+    // Find the element that was clicked
+    const target = (e.target as HTMLElement).closest(".tooltip");
+
+    if (!target) return;
+
+    // Determine the position of the element
+    const x = target.clientLeft + target.clientWidth;
+    const y = target.clientTop + target.clientHeight;
+
+    // Show the context menu
+    const contextMenu = document.createElement("div");
+    contextMenu.style.position = "absolute";
+    contextMenu.style.left = `${x}px`;
+    contextMenu.style.top = `${y}px`;
+    contextMenu.innerHTML = `
+      <ul>
+        <li><a href="#" on:click={editNode}>Edit</a></li>
+        <li><a href="#" on:click={addNode}>Add</a></li>
+      </ul>
+    `;
+    document.body.appendChild(contextMenu);
+
+    // Handle clicks outside the context menu to close it
+    function handleOutsideClick(event: MouseEvent) {
+      if (!contextMenu.contains(event.target as Node)) {
+        contextMenu.remove();
+        window.removeEventListener("click", handleOutsideClick);
+      }
+    }
+    window.addEventListener("click", handleOutsideClick);
+  };
 </script>
 
-<div class="node">
+<div class="node" on:contextmenu={handleContextMenu}>
   {#if !!label}
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="tooltip" on:click={clickHandler}>
@@ -42,10 +80,6 @@
   .list {
     list-style: none;
   }
-  .node {
-    cursor: pointer;
-    width: max-content;
-  }
   .tooltip:hover {
     background-color: var(--primary-color);
     color: #fff;
@@ -57,5 +91,12 @@
     -moz-user-select: none; /* Firefox */
     -ms-user-select: none; /* Internet Explorer/Edge */
     user-select: none; /* Non-prefixed version */
+  }
+
+  .node {
+    width: 100%;
+    cursor: pointer;
+    width: max-content;
+    padding: 5px;
   }
 </style>
